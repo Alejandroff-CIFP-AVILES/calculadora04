a738b532add29c2d7106bf970f2a5f89606fdb5b
Alejandro Fernandez <HTX45221@educastur.es>
2022-05-06 21:05:44 +0200

    Subir jenkinsfile 5

--- Jenkinsfile
+++ Jenkinsfile
@@ -6,12 +6,6 @@ pipeline {
 				git url: 'https://github.com/Alejandroff-CIFP-AVILES/calculadora04', branch: "main"
 			}
 		}	
-
-		stage("Unit test"){
-			steps {
-			
-				}
-			}
 		
 		stage("Unit test"){
 			steps {
--- calculadora.py
+++ calculadora.py
@@ -1,14 +1,17 @@
 
-
+import sys
 def main():
+
 	print("Calculadora04")
-	numero1 = int(input("Dígame el primer número: "))
-	numero2 = int(input(f"Dígame el primer número: "))
+	numero1 = int(sys.argv[1])
+	numero2 = int(sys.argv[2])
 	print(f"La multiplicacion es: {multiplicacion(numero1, numero2)}.")
 
 def multiplicacion(numero1 , numero2):
 	return numero1 * numero2
 
 if __name__ == "__main__":
+
     main()
+
 
